b88e7c42a740804419b98b632ede0c4b7b10bf31
Unknown <andrewrominger@outlook.com>
2017-04-29 09:21:59 -0700

    Bug Fixes, Comment Work

---
+++ app/src/main/java/com/sellit/testdrawer/Comment.java
@@ -0,0 +1,8 @@
+package com.sellit.testdrawer;
+
+/**
+ * Created by Andrew on 4/29/2017.
+ */
+
+public class Comment {
+}
---
+++ app/src/main/java/com/sellit/testdrawer/CommentFragment.java
@@ -0,0 +1,10 @@
+package com.sellit.testdrawer;
+
+import android.app.Fragment;
+
+/**
+ * Created by Andrew on 4/29/2017.
+ */
+
+public class CommentFragment extends Fragment {
+}
