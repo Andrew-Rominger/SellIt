087fb6cdffad5d6f14926bdad1e26208bda0f580
Unknown <coltonjjacobson@gmail.com>
2017-04-29 10:46:14 -0700

    DonateRecycle

--- app/src/main/java/com/sellit/testdrawer/DonateFragment.java
+++ app/src/main/java/com/sellit/testdrawer/DonateFragment.java
@@ -1,14 +1,53 @@
 package com.sellit.testdrawer;
 
-        import android.app.Fragment;
-        import android.os.Bundle;
-        import android.support.annotation.Nullable;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.TabHost;
+import android.app.Fragment;
+import android.graphics.Movie;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TabHost;
 
 public class DonateFragment extends Fragment {
+
+package info.androidhive.recyclerview;
+
+    public class donatedItem {
+        private String name, descirption, condition, year;
+
+        public string (name) {
+        }
+
+        public Movie(String title, String genre, String year) {
+            this.title = title;
+            this.genre = genre;
+            this.year = year;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String name) {
+            this.title = name;
+        }
+
+        public String getYear() {
+            return year;
+        }
+
+        public void setYear(String year) {
+            this.year = year;
+        }
+
+        public String getUSername() {
+            return genre;
+        }
+
+        public void setGenre(String genre) {
+            this.genre = genre;
+        }
 
     View myView;
 
